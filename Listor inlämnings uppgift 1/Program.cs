af58c723b87d78a02ad709a7dec048201112e575
dean06001 <dean06001@utb.vaxjo.se>
2024-02-02 12:31:09 +0100

    Add project files.

---
+++ Listor inlämnings uppgift 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> namnLista = new List<string>();
+
+        while (true)
+        {
+            Console.Write("Ange ett namn (Tryck enter för att sluta):");
+            string namn = Console.ReadLine();
+
+            if (namn == null || namn == "")
+            {
+                break;
+            }
+            else
+            {
+                namnLista.Add(namn);
+            }
+        }
+
+        Console.WriteLine("Nedan är namnen du angav i omvänd ordning:");
+        namnLista.Sort();
+        namnLista.Reverse();
+        foreach (string namn in namnLista)
+        {
+            Console.WriteLine(namn);
+        }
+        Console.ReadLine();
+    }
+}
